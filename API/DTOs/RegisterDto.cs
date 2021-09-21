303610fb77487fd9ea58ebded69d10714c3586fe
Tom Hine <tomhine792@gmail.com>
2021-09-21 23:01:37 +0100

    End of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,7 +6,9 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
+        
         [Required]
+        [StringLength(16 ,MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
